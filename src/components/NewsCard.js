7362b36637d03e31ef7b97c85554994a957f258a
mdayal385 <mdayal385@gmail.com>
2023-01-18 22:15:14 +0530

    cards for newsletter.

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React from 'react'; 
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 const NewsCard = ({article}) => {
     if(!article.title) return null;
   return (
-    <div className='news-card'>
-        <h3>{article.title}</h3>
-        <a href={article.url}>Read More</a>
-    </div>
+    <Box className='news-card' sx={{width:'75%', pl: 3, pb:2}} >
+        <Card variant='outlined'>
+        <React.Fragment>
+      <CardContent>
+        <Typography  variant="h6" color="text.primary" gutterBottom>
+        {article.title}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" href={article.url}>Read More</Button>
+      </CardActions>
+     </React.Fragment>
+        </Card>
+    </Box>
   );
 };
 
